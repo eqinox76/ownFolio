9254a4a40f158f5354b9fcdbc0d9d794ce57e3e8
Carsten Dänschel <carsten@daenschel.net>
2015-12-29 18:06:17 +0100

    added unittest for memcache usage when getting data

--- backend/historicData.go
+++ backend/historicData.go
@@ -1,7 +1,65 @@
-package backend
+package timeseries
 
-import "data"
+import (
+	"log"
+	"encoding/gob"
+	"bytes"
+	"../data"
+	"../importers"
+	"appengine/memcache"
+	"appengine"
+)
 
-func GetHistoricInstrument(id string) data.Instrument {
+func getDataF(id string) (data.Instrument, error){
+	url := importers.GenerateURL("YAHOO", id)
+	return importers.GetHistory(url)
+}
 
+var getData = getDataF
+
+
+func GetInstrument(id string, ctx appengine.Context) data.Instrument {
+	// get item from memcache
+	if item, err := memcache.Get(ctx, id); err == memcache.ErrCacheMiss {
+		// item not in cache
+		instr, err := getData(id)
+		if err != nil{
+			log.Panicf("could not get data for %s because %s", id, err)
+		}
+		var buffer bytes.Buffer
+		enc := gob.NewEncoder(&buffer)
+		err = enc.Encode(instr)
+		if err != nil{
+			log.Panicf("could not encode %s", instr)
+		}
+		cacheEntry := &memcache.Item{
+			Key: id,
+			Value: buffer.Bytes(),
+		}
+		err = memcache.Add(ctx, cacheEntry)
+		if err != nil{
+			log.Printf("something was wrong when adding a new item %s err:%s", ctx, err)
+		}
+		return instr
+	} else if err != nil {
+		log.Panicf("error getting id:%s err:%s", id, err)
+		instr, err := getData(id)
+		if err != nil{
+			log.Panicf("could not get data for %s because %s", id, err)
+		}
+		return instr
+	} else {
+		buffer := bytes.NewBuffer(item.Value)
+		var instr data.Instrument
+		dec := gob.NewDecoder(buffer)
+		err := dec.Decode(&instr)
+		if err != nil{
+			log.Panicf("Could not decode data for %s err: %s", id, err)
+			instr, err = getData(id)
+			if err != nil{
+				log.Panicf("could not get data for %s because %s", id, err)
+			}
+		}
+		return instr
+	}
 }
---
+++ backend/historicData_test.go
@@ -0,0 +1,39 @@
+package timeseries
+
+import (
+	"../data"
+	"log"
+	"testing"
+	"appengine/aetest"
+)
+
+func TestGetInstrument(t *testing.T){
+	called := 0
+	getData = func (id string) (data.Instrument, error){
+		log.Printf("overwritten method called with id %s", id)
+		var i data.Instrument
+		called++
+		return i, nil
+	}
+
+	ctx, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer ctx.Close()
+
+	GetInstrument("DAX", ctx)
+
+	if called != 1{
+		t.Fatal("Calling once produced %i called", called)
+	}
+
+	GetInstrument("DAX", ctx)
+	GetInstrument("DAX", ctx)
+	GetInstrument("DAX", ctx)
+
+	if called != 1{
+		t.Fatal("Calling once produced %i called", called)
+	}
+}
