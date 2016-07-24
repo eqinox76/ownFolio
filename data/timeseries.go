4d29f5e023a4563d6ed80eb0403925a107ed4d4c
Carsten Daenschel <carsten@daenschel.net>
2016-07-24 19:11:51 +0200

    cleanup and docu

--- data/timeseries.go
+++ data/timeseries.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// descripes the value for a certain day for a certain instrument
 type DataPoint struct {
 	Time   time.Time
 	High   float32
@@ -17,20 +18,25 @@ func (d DataPoint) String() string {
 	return fmt.Sprintf("%s high:%f low:%f close:%f vol:%d", d.Time.Format("2006-01-02"), d.High, d.Low, d.Close, d.Volume)
 }
 
+// wrapper for simplicity
 type DataPoints []DataPoint
 
+// needed for sorting TODO: boilerplate
 func (slice DataPoints) Len() int {
 	return len(slice)
 }
 
+// needed for sorting
 func (slice DataPoints) Less(i, j int) bool {
 	return slice[i].Time.Before(slice[j].Time)
 }
 
+// needed for sorting
 func (slice DataPoints) Swap(i, j int) {
 	slice[i], slice[j] = slice[j], slice[i]
 }
 
+// describes a instrument with all known values on day basis
 type Instrument struct {
 	Name string
 	ISIN string
@@ -39,22 +45,4 @@ type Instrument struct {
 
 func (instr *Instrument) Add(p *DataPoint) {
 	instr.Data = append(instr.Data, *p)
-}
-
-type C3Data struct{
-	Date []string
-	High []float32
-	Low []float32
-	Close []float32
-}
-
-func (instr *Instrument) C3Data() C3Data{
-	var result C3Data
-	for _, value := range instr.Data {
-		result.Date = append(result.Date, value.Time.Format("2006-01-02"))
-		result.High = append(result.High, value.High)
-		result.Low = append(result.Low, value.Low)
-		result.Close = append(result.Close, value.Close)
-	}
-	return result
 }
