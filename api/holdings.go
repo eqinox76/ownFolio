57ad8e2050298d9b32e1b01dc017068fd1fb886f
Carsten Daenschel <carsten@daenschel.net>
2016-07-26 22:05:06 +0200

    further moving of functionality from main.go to its own api areas

--- api/holdings.go
+++ api/holdings.go
@@ -1,1 +1,81 @@
 package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"appengine/datastore"
+
+	"github.com/eqinox76/ownFolio/data"
+)
+
+func GetHolding(w http.ResponseWriter, r *http.Request) {
+	c, _, login := CheckLogin(w, r)
+	if !login {
+		http.Error(w, "Not logged in correctly", 401)
+	}
+
+	q := datastore.NewQuery("holding")
+	var results []data.Holding
+	_, err := q.GetAll(c, &results)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	jsData, err := json.Marshal(results)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	fmt.Fprintf(w, "%s", jsData)
+
+}
+
+// http://localhost:8080/holding/add?isin=%22huhuh%22&price=42.4&volume=51223&date=%222015-01-04%22
+func AddHolding(w http.ResponseWriter, r *http.Request) {
+	c, _, login := CheckLogin(w, r)
+	if !login {
+		http.Error(w, "Not logged in correctly", 401)
+	}
+
+	isin := strings.Trim(r.URL.Query().Get("isin"), "\"")
+	priceStr := strings.Trim(r.URL.Query().Get("price"), "\"")
+	dateStr := strings.Trim(r.URL.Query().Get("date"), "\"")
+	volumeStr := strings.Trim(r.URL.Query().Get("volume"), "\"")
+
+	if isin == "" || priceStr == "" || dateStr == "" || volumeStr == "" {
+		fmt.Fprintf(w, "%v\n", r.URL.Query())
+		http.Error(w, "Empty/missing parameters. Need isin, price, date, volume", http.StatusBadRequest)
+		return
+	}
+
+	price, err := strconv.ParseFloat(priceStr, 64)
+	if err != nil {
+		http.Error(w, "Could not parse "+priceStr, http.StatusBadRequest)
+		return
+	}
+
+	volume, err := strconv.ParseFloat(volumeStr, 64)
+	if err != nil {
+		http.Error(w, "Could not parse "+volumeStr, http.StatusBadRequest)
+		return
+	}
+
+	date, err := time.Parse("2006-01-02", dateStr)
+	if err != nil {
+		http.Error(w, "Could not parse '"+dateStr+"'\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// we add a new symbol
+	a := data.Holding{ISIN: isin, Price: price, Volume: volume, BuyDate: date}
+	_, err = datastore.Put(c, datastore.NewIncompleteKey(c, "holding", nil), &a)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+}
--- main.go
+++ main.go
@@ -6,10 +6,6 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
-	"strings"
-	"time"
-
-	"appengine/datastore"
 
 	"github.com/eqinox76/ownFolio/api"
 	"github.com/eqinox76/ownFolio/data"
@@ -19,35 +15,13 @@ func init() {
 	http.HandleFunc("/stocks", getStock)
 	http.HandleFunc("/timeseries", getTimeSeries)
 	http.HandleFunc("/holding", showHoldings)
-	http.HandleFunc("/holding/add", addHolding)
-	http.HandleFunc("/holding/get", getHolding)
+	http.HandleFunc("/holding/add", api.AddHolding)
+	http.HandleFunc("/holding/get", api.GetHolding)
 }
 
 var chartTempl = template.Must(template.ParseFiles("templates/base.html", "templates/chart.html"))
 
 var holdingTempl = template.Must(template.ParseFiles("templates/base.html", "templates/holding.html"))
-
-func getHolding(w http.ResponseWriter, r *http.Request) {
-	c, _, login := api.CheckLogin(w, r)
-	if !login {
-		http.Error(w, "Not logged in correctly", 401)
-	}
-
-	q := datastore.NewQuery("holding")
-	var results []data.Holding
-	_, err := q.GetAll(c, &results)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	jsData, err := json.Marshal(results)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	fmt.Fprintf(w, "%s", jsData)
-
-}
 
 func showHoldings(w http.ResponseWriter, r *http.Request) {
 	_, _, login := api.CheckLogin(w, r)
@@ -59,51 +33,6 @@ func showHoldings(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-}
-
-// http://localhost:8080/holding/add?isin=%22huhuh%22&price=42.4&volume=51223&date=%222015-01-04%22
-func addHolding(w http.ResponseWriter, r *http.Request) {
-	c, _, login := api.CheckLogin(w, r)
-	if !login {
-		http.Error(w, "Not logged in correctly", 401)
-	}
-
-	isin := strings.Trim(r.URL.Query().Get("isin"), "\"")
-	priceStr := strings.Trim(r.URL.Query().Get("price"), "\"")
-	dateStr := strings.Trim(r.URL.Query().Get("date"), "\"")
-	volumeStr := strings.Trim(r.URL.Query().Get("volume"), "\"")
-
-	if isin == "" || priceStr == "" || dateStr == "" || volumeStr == "" {
-		fmt.Fprintf(w, "%v\n", r.URL.Query())
-		http.Error(w, "Empty/missing parameters. Need isin, price, date, volume", http.StatusBadRequest)
-		return
-	}
-
-	price, err := strconv.ParseFloat(priceStr, 64)
-	if err != nil {
-		http.Error(w, "Could not parse "+priceStr, http.StatusBadRequest)
-		return
-	}
-
-	volume, err := strconv.ParseFloat(volumeStr, 64)
-	if err != nil {
-		http.Error(w, "Could not parse "+volumeStr, http.StatusBadRequest)
-		return
-	}
-
-	date, err := time.Parse("2006-01-02", dateStr)
-	if err != nil {
-		http.Error(w, "Could not parse '"+dateStr+"'\n"+err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// we add a new symbol
-	a := data.Holding{ISIN: isin, Price: price, Volume: volume, BuyDate: date}
-	_, err = datastore.Put(c, datastore.NewIncompleteKey(c, "holding", nil), &a)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
 }
 
 func getStock(w http.ResponseWriter, r *http.Request) {
